061be8be757797d634f61e3005784cf264454bf7
RenanCampos1 <r.campos_@outlook.com>
2023-06-12 12:41:47 -0300

    css inline

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -14,7 +14,9 @@ export default class DetailProduct extends Component {
   }
 
   detailsProduct = async () => {
-    const { match: { params: { id } } } = this.props;
+    const {
+      match: { params: { id } },
+    } = this.props;
     const result = await getProductDetails(id);
     this.setState({
       data: result,
@@ -22,29 +24,81 @@ export default class DetailProduct extends Component {
   };
 
   render() {
-    const { data: { title, thumbnail, price, warranty, id } } = this.state;
+    const {
+      data: { title, thumbnail, price, warranty, id },
+    } = this.state;
     return (
       <div>
-        <img data-testid="product-detail-image" src={ thumbnail } alt={ title } />
-        <p data-testid="product-detail-name">{title}</p>
-        <span data-testid="product-detail-price">{`R$ ${price}`}</span>
-        <p>{warranty}</p>
-        <Link to="/cart">
-          <button
-            data-testid="shopping-cart-button"
-            type="button"
-          >
-            Comprar
-          </button>
-        </Link>
-        <button
-          data-testid="product-detail-add-to-cart"
-          type="button"
-          onClick={ () => addFavorites(title, price, id) }
-        >
-          Adicionar ao carrinho
-        </button>
+        <style>
+          {`
+            .product-detail {
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              text-align: center;
+            }
 
+            .product-detail-image {
+              width: 200px;
+              margin-bottom: 10px;
+            }
+
+            .product-detail-name {
+              font-size: 20px;
+              font-weight: bold;
+            }
+
+            .product-detail-price {
+              font-size: 18px;
+              font-weight: bold;
+            }
+
+            .product-detail-warranty {
+              margin-top: 10px;
+            }
+
+            .product-detail-buttons {
+              margin-top: 20px;
+            }
+
+            .product-detail-buttons button {
+              padding: 8px 16px;
+              font-size: 16px;
+              background-color: #4CAF50;
+              color: white;
+              border: none;
+              cursor: pointer;
+              margin-right: 10px;
+            }
+          `}
+        </style>
+        <div className="product-detail">
+          <img
+            data-testid="product-detail-image"
+            className="product-detail-image"
+            src={thumbnail}
+            alt={title}
+          />
+          <p data-testid="product-detail-name" className="product-detail-name">
+            {title}
+          </p>
+          <span data-testid="product-detail-price" className="product-detail-price">{`R$ ${price}`}</span>
+          <p className="product-detail-warranty">{warranty}</p>
+          <div className="product-detail-buttons">
+            <Link to="/cart">
+              <button data-testid="shopping-cart-button" type="button">
+                Comprar
+              </button>
+            </Link>
+            <button
+              data-testid="product-detail-add-to-cart"
+              type="button"
+              onClick={() => addFavorites(title, price, id)}
+            >
+              Adicionar ao carrinho
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,6 @@ class Home extends React.Component {
     messageOn: false,
     lista: [],
     products: [],
-
   };
 
   componentDidMount() {
@@ -54,71 +53,151 @@ class Home extends React.Component {
     const { productName, messageOn, lista, products: { results } } = this.state;
     return (
       <div>
-        <input
-          type="text"
-          name="productName"
-          value={ productName }
-          onChange={ this.handleChange }
-          data-testid="query-input"
-        />
-        <button
-          data-testid="query-button"
-          type="button"
-          onClick={ this.requireProducts }
-        >
-          Pesquisarr
-        </button>
-        {
-          productName.length === 0
-          && (
-            <p data-testid="home-initial-message">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-            </p>)
-        }
-        <button
-          data-testid="shopping-cart-button"
-          type="button"
-          onClick={ this.handleClick }
-        >
-          Carrinho de compras
+        <style>
+          {`
+            .container {
+              max-width: 800px;
+              margin: 0 auto;
+              padding: 20px;
+            }
 
-        </button>
-        {
-          messageOn && <Redirect to="/Cart" />
-        }
-        { results !== undefined ? results.map(({ id, title, thumbnail, price }) => (
-          <div key={ id }>
-            <Link data-testid="product-detail-link" key={ id } to={ `/detail/${id}` }>
-              {' '}
-              <div key={ id } data-testid="product">
-                {title}
-                <img src={ thumbnail } alt={ title } />
-                {price}
-              </div>
-            </Link>
+            .search-container {
+              display: flex;
+              align-items: center;
+              margin-bottom: 20px;
+            }
+
+            .search-input {
+              flex: 1;
+              margin-right: 10px;
+              padding: 5px;
+              font-size: 16px;
+            }
+
+            .search-button {
+              padding: 8px 16px;
+              font-size: 16px;
+              background-color: #4CAF50;
+              color: white;
+              border: none;
+              cursor: pointer;
+            }
+
+            .initial-message {
+              margin-bottom: 20px;
+              font-size: 16px;
+            }
+
+            .cart-button {
+              padding: 8px 16px;
+              font-size: 16px;
+              background-color: #FF9800;
+              color: white;
+              border: none;
+              cursor: pointer;
+            }
+
+            .product {
+              display: flex;
+              align-items: center;
+              margin-bottom: 20px;
+              font-size: 16px;
+            }
+
+            .product-image {
+              width: 80px;
+              margin-right: 10px;
+            }
+
+            .product-title {
+              flex: 1;
+            }
+
+            .product-price {
+              font-weight: bold;
+            }
+
+            .category-label {
+              display: block;
+              margin-bottom: 10px;
+              font-size: 16px;
+            }
+
+            .category-input {
+              margin-right: 5px;
+            }
+          `}
+        </style>
+        <div className="container">
+          <div className="search-container">
+            <input
+              className="search-input"
+              type="text"
+              name="productName"
+              value={productName}
+              onChange={this.handleChange}
+              data-testid="query-input"
+            />
             <button
-              data-testid="product-add-to-cart"
+              className="search-button"
+              data-testid="query-button"
               type="button"
-              onClick={ () => addFavorites(title, price, id) }
+              onClick={this.requireProducts}
             >
-              Adicionar ao carrinho
+              Pesquisar
             </button>
           </div>
-        )) : <p>Nenhum produto foi encontrado</p>}
-        {lista.map((e) => (
-          <label htmlFor="radio" key={ e.id } data-testid="category">
-            { e.name }
-            <input
-              onClick={ this.requestCategoryID }
-              type="radio"
-              name="categories"
-              id="radio"
-              value={ e.id }
-              key={ e.id }
-            />
-          </label>))}
+          {productName.length === 0 && (
+            <p className="initial-message" data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          )}
+          <button
+            className="cart-button"
+            data-testid="shopping-cart-button"
+            type="button"
+            onClick={this.handleClick}
+          >
+            Carrinho de compras
+          </button>
+          {messageOn && <Redirect to="/Cart" />}
+          {results !== undefined ? (
+            results.map(({ id, title, thumbnail, price }) => (
+              <div key={id} className="product">
+                <Link data-testid="product-detail-link" key={id} to={`/detail/${id}`}>
+                  <div className="product-title">{title}</div>
+                  <img className="product-image" src={thumbnail} alt={title} />
+                  <div className="product-price">{price}</div>
+                </Link>
+                <button
+                  className="product-add-to-cart"
+                  data-testid="product-add-to-cart"
+                  type="button"
+                  onClick={() => addFavorites(title, price, id)}
+                >
+                  Adicionar ao carrinho
+                </button>
+              </div>
+            ))
+          ) : (
+            <p>Nenhum produto foi encontrado</p>
+          )}
+          {lista.map((e) => (
+            <label htmlFor="radio" key={e.id} className="category-label" data-testid="category">
+              {e.name}
+              <input
+                className="category-input"
+                onClick={this.requestCategoryID}
+                type="radio"
+                name="categories"
+                id="radio"
+                value={e.id}
+                key={e.id}
+              />
+            </label>
+          ))}
+        </div>
       </div>
-
     );
   }
 }
